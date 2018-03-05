8dcb21119d23fe7386fa6d027b77c6fc58ef472b
matt lubel <matt_lubel@hotmail.com>
2018-03-05 19:58:49 +0100

    get the db working ;)'

--- db.js
+++ db.js
@@ -1,18 +1,25 @@
-var MongoClient = require( 'mongodb' ).MongoClient
+const MongoClient = require('mongodb').MongoClient
 const DB_URL = 'mongodb://localhost:27017/stripper'
+const { promisify } = require('util')
+const connect = promisify(MongoClient.connect)
 
-let _db
+;(async () => {
+	const client = await connect(DB_URL)
+	const db = client.db('stripper');
+	console.log("Connected successfully to db");
 
-module.exports = {
+	const posts = await db.collection('posts')
+	// await posts.insert({test: 'string'})
+	// console.log('success')
 
-  connect: function(callback) {
-    MongoClient.connect( DB_URL, function( err, db ) {
-      _db = db
-      return callback( err )
-    } )
-  },
+	const searchText = '"you should search it with any questions"'
 
-  getDb: function() {
-    return _db
-  }
-}
+	const results = await posts.find({ $text: { $search: searchText } }).toArray()
+
+	// db.posts.createIndex({ "text":"text", "comments.content":"text" })
+
+	// const all = await posts.find({}).toArray()
+	console.log(results)
+
+	client.close()
+})()
