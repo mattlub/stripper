8e3d33ee00d3c7a9eb97f8928783355f9efbebd9
matt lubel <matt_lubel@hotmail.com>
2018-03-19 20:29:20 +0100

    update

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 Scrapes posts from a facebook group and adds them to a MongoDb database.
-Using Puppeteer.
 
-Todo:
-- click through 'see previous' or 'see more' (comments) when there are many comments, in order to collect all comments
-- convert puppeteer evaluate methods to other methods to allow for error handling (which should be implemented)
-- allow skipping of certain number of pages if restarting the scrape
-- reliably collect likes/reactions
+#### Instructions for use:
+- find the url of the facebook group
+- change the `GROUP_PAGE_URL` variable in `index.js` (you must prepend `mbasic` to the url, as in the example) 
+- amend the `DB_URL`, `NUM_PAGES` and `NUM_TO_SKIP` variables if you like
+- if the group requires you to log in to view it, create a file `./secrets.js` containing
+```js
+module.exports = {
+  email: <your_email>,
+  pass: <your_pass>
+}
+```
+- you must have a mongodb running- type `mongod`
+- run `node index.js` to start the scrape
+
+#### Currently stores:
+- posts
+- all comments for each post
+
+#### Currently does not store:
+- date
+- likes/reactions for posts/comments
+- nested comments
+- 'special' posts e.g. recommendations, pictures, links
+
+#### Notes:
+- If you are an admin you can use the facebook API to fetch posts, but this is for use of well-meaning non-admins :)
+- This will by no means get all events, because of _the facebook algorithm_
+
+#### Todo:
+- [x] click through 'see previous' or 'see more' (comments) when there are many comments, in order to collect all comments
+- [] convert puppeteer evaluate methods to other methods to allow for error handling (which should be implemented)
+- [x] allow skipping of certain number of pages if restarting the scrape
+- [] reliably collect likes/reactions
+
+
+#### Contributions welcome :)
--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const MongoClient = require('mongodb').MongoClient
 const puppeteer = require('puppeteer')
 const { promisify } = require('util')
 
-const { querySelectArray, maybeFindText } = require('./utils')
-const { email, pass } = require('./secrets')
-
 const connect = promisify(MongoClient.connect)
 
 const DB_URL = 'mongodb://localhost:27017/stripper'
 const GROUP_PAGE_URL = 'https://mbasic.facebook.com/groups/228761190470008?bacr=1519989551%3A1839721619373949%3A1839721619373949%2C0%3A7%3A&multi_permalinks&refid=18'
 const BASE_URL = 'https://mbasic.facebook.com'
-const NUM_PAGES = 500
+const NUM_PAGES = 10000
+const NUM_TO_SKIP = 0
 
 const posts = []
 const tally = {
@@ -32,130 +30,235 @@ const tally = {
   const detailPage = await browser.newPage()
 
   try {
-    // start
-    await page.goto(GROUP_PAGE_URL)
-
-    //login
+    // go to page and log in if necessary.
+    await Promise.all([
+      page.goto(GROUP_PAGE_URL),
+      page.waitForNavigation()
+    ])
+    // console.log('gone to page')
+    // console.log('awaited navigation')
+    //login if necessary
     const emailInput = await page.$('input[name="email"]')
     if (emailInput) {
+      const { email, pass } = require('./secrets')
+      if (!email || !pass) {
+        throw new Error('please export "email" and a "pass" variables from ./secrets.js')
+      }
       console.log('logging in...')
       await page.evaluate((email, pass) => {
         document.querySelector('input[name="email"]').value = email
         document.querySelector('input[name="pass"]').value = pass
         // German lol
+        // TODO: define language at the top
         document.querySelector('input[value="Anmelden"]').click()
       }, email, pass)
+      console.log('logged in.')
     }
+  } catch (err) {
+    console.log('error loading page and logging in: ', err)
+  }
+  
+  // TODO: change this because 
+  // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageclickselector-options
+  await page.waitForNavigation()
 
-    for (let ii = 0; ii < NUM_PAGES; ii++) {
+  try {
+    for (let ii =0; ii < NUM_PAGES; ii++) {
       // wait for load
-      await page.waitFor(3000)
+      // console.log('scraping page', ii + 1)
+
+      // currently copied from the bottom
+      if (ii < NUM_TO_SKIP) {
+        // click the see more button to load a new page of posts
+        const seeMoreLink = await page.evaluate(() => {
+          const seeMore = Array.prototype.slice.call(document.querySelectorAll('a'))
+          .find(a => a.querySelector('span') && a.querySelector('span').textContent === 'See more posts')
+          return seeMore.href
+        })
+        await Promise.all([
+          page.waitForNavigation(),
+          page.goto(seeMoreLink)
+        ])
+        console.log('skipped page number', ii)
+        continue
+      }
 
       // take the links off the page
-      let storyLinks
-      storyLinks = await page.evaluate(() => {
+      const storyLinks = await page.evaluate(() => {
         const arr = Array.prototype.slice.call(document.querySelectorAll('a'))
         return arr
           .filter(a => a.textContent === 'Full Story')
           .map(a => a.href)
       })
 
-      // log all the posts
+      // loop through the individual posts
       for (let i = 0; i < storyLinks.length; i++) {
         const link = storyLinks[i]
-        // console.log(link)
+        // const link = 'https://mbasic.facebook.com/groups/228761190470008?view=permalink&id=1838622442817200&p=0&av=760465452&refid=18'
+        // console.log('---------')
         await detailPage.goto(link)
-        await detailPage.screenshot({path: 'example.png'})
-        const post = await detailPage.evaluate(() => {
-          const querySelectArray = selector => Array.prototype.slice.call(document.querySelectorAll(selector))
-          const maybeFindText = function(inside, selector) {
-            const [ins , sel] = arguments.length === 2 
-              ? [inside, selector]
-              : [document, inside]
-            return ins.querySelector(sel) && ins.querySelector(sel).textContent
-          }
+        // await detailPage.screenshot({path: './example.png'})
 
-          // try to find the title text...
-          const textClasses = ['bo', 'bp', 'bq', 'br']
-          let jj = 0
-          let text = []
-          do {
-            text = maybeFindText(`.${textClasses[jj]} > p`)
-            jj = jj + 1
-          } while (jj < textClasses.length && !text)
-          console.log('last checked title class ', textClasses[jj - 1])
-
-          const post = {
-            title: maybeFindText('h3'),
-            text,
-            likes: maybeFindText('.co .cs'),
-            date: maybeFindText('.bq abbr') || maybeFindText('.bs abbr')
-          }
-
-          const commentsAreWrong = cx =>
-            !cx.length ||
-            !cx[0].id
-
-          // try to find the comments...
-          const commentsClasses = ['cn', 'co', 'cp', 'cq', 'cr', 'cs', 'ct', 'cu', 'cv', 'cw', 'cx', 'cy']
-          let j = 0
-          let comments = []
-          do {
-            comments =
-              querySelectArray(`div.${commentsClasses[j]}`)
-              .filter(c => /^[0-9]*$/.test(c.id))
-
-            j = j + 1
-          } while (j < commentsClasses.length && commentsAreWrong(comments))
-          console.log('last checked comment class ', commentsClasses[j - 1])
-
-          comments = comments
-            .map(c => {
-              const author = maybeFindText(c, 'h3 > a')
-              let contentClasses = ['cn', 'co', 'cp', 'cq', 'cr', 'cs', 'ct', 'cu', 'cv', 'cw', 'cx', 'cy']
-              let k = 0
-              do {
-                content = maybeFindText(c, `div.${contentClasses[k]}`)
-                k = k + 1
-              }
-              while (k < contentClasses.length && (!content || content === author))
-              console.log('last checked comment content class ', commentsClasses[k - 1])
-
-              return {
-                author,
-                content,
-                likes: maybeFindText(c, '.cx .db')
-              }
-            })
-            .filter(c => !(c.author === null && c.content === null))
-          
-          return {
-            ...post,
-            comments
-          }
-        })
-        // log 
-        const vital = {
-          link,
-          title: post.title,
-          text: post.text,
-          commenter: post.comments[0] && post.comments[0].author,
-          comment: post.comments[0] && post.comments[0].content
+        // try to find the post title...
+        const titleHandle = await detailPage.$(`h3 a`)
+        let postTitle
+        if (titleHandle) {
+          const innerTextHandle = await titleHandle.getProperty('innerText')
+          postTitle = await innerTextHandle.jsonValue()
         }
+        // if (postTitle) {
+        //   console.log('found post title:', postTitle.slice(0, 20), '...')
+        // } else {
+        //   console.log('no post title found.')
+        // }
 
-        if (Object.values(vital).some(val => !val)) {
-          // console.log(vital, '\n')
+        // try to find the post text...
+        let postTextClass
+        const postTextClasses = ['bo', 'bp', 'bq', 'br']
+        let jj = 0
+        let postText
+        do {
+          postTextClass = postTextClasses[jj]
+          const textHandle = await detailPage.$(`.${postTextClass} > p`)
+          if (textHandle) {
+            const innerTextHandle = await textHandle.getProperty('innerText')
+            postText = await innerTextHandle.jsonValue()
+          }
+          jj++
+        } while (jj < postTextClasses.length && !postText)
+        // if (postText) {
+        //   console.log('found post text:', postText.slice(0, 20), '...')
+        // } else {
+        //   console.log('no post text found.')
+        // }
+
+        // try to find the comments class...
+        let commentsClass
+        const commentsClasses = ['cn', 'co', 'cp', 'cq', 'cr', 'cs', 'ct', 'cu', 'cv', 'cw', 'cx', 'cy']
+        let j = 0
+        let comments = []
+        do {
+          commentsClass = commentsClasses[j]
+          comments = await detailPage.evaluate(commentsClass => {
+            return Array.prototype.slice.call(document.querySelectorAll(`div.${commentsClass}`))
+              .filter(c => c.id && /^[0-9]*$/.test(c.id))
+          }, commentsClass)
+          j = j + 1
+        } while (j < commentsClasses.length && comments.length === 0)
+        // if (comments.length) {
+        //   console.log('comments class found:', commentsClass)
+        // } else {
+        //   console.log('no comments found.')
+        // }
+
+        // find comments content class
+        let commentContentClass
+        let commentContentClasses = ['cn', 'co', 'cp', 'cq', 'cr', 'cs', 'ct', 'cu', 'cv', 'cw', 'cx', 'cy']
+        let k = 0
+        do {
+          commentContentClass = commentContentClasses[k]
+          content = await detailPage.evaluate((commentsClass, commentContentClass) => {
+            const contentNodes =
+              document.querySelectorAll(`div.${commentsClass} > div > div.${commentContentClass}`)
+            return Array.prototype.slice.call(contentNodes)
+              .filter(c => !c.id && c.textContent && c.textContent.length)
+          }, commentsClass, commentContentClass)
+          k = k + 1
+        }
+        while (k < commentContentClasses.length && !content.length)
+        // if (content.length) {
+        //   console.log('comment contents class found:', commentContentClass)
+        // } else {
+        //   console.log('no comment content class found.')
+        // }
+
+        // find numCommentsPages
+        let numCommentPages = 1
+        let seePrevLink 
+        do {
+          // console.log('looking for see prev link.')
+          seePrevLink = await detailPage.evaluate(commentsClass => {
+            const commentNodes = document.querySelectorAll(`div.${commentsClass}`)
+            const seePrev = Array.prototype.slice.call(commentNodes)
+              .find(c => c.id.includes('see_prev'))
+            if (seePrev) {
+              const link = seePrev.querySelector('a')
+              return link && link.href
+            }
+            return null
+          }, commentsClass)
+  
+          if (seePrevLink) {
+            // console.log('found see previous link.')
+            numCommentPages ++
+            await Promise.all([
+              detailPage.waitForNavigation(),
+              detailPage.goto(seePrevLink)
+            ])
+          }
+        } while (seePrevLink)
+        // console.log('number of pages of comments:', numCommentPages)
+
+        // find comments again now we know how many pages there are.
+        let allComments = []
+        for (let i = 0; i < numCommentPages; i++) {
+          // find comments
+          comments = await detailPage.evaluate((commentsClass, commentContentClass) => {
+            const comments = Array.prototype.slice.call(document.querySelectorAll(`div.${commentsClass}`))
+              .filter(c => c.id && /^[0-9]*$/.test(c.id))
+            return comments.map(c => {
+              const author = c.querySelector('h3 > a') && c.querySelector('h3 > a').textContent
+              const contentNode = c.querySelector(`div.${commentContentClass}`)
+              const content = contentNode && contentNode.textContent
+              return { author, content }
+            })
+          }, commentsClass, commentContentClass)
+
+          // add them to 'allComments' array
+          allComments = allComments.concat(comments)
+          if (i === numCommentPages - 1) {
+            break
+          }
+          // click see next
+          seeNextLink = await detailPage.evaluate(commentsClass => {
+            const commentNodes = document.querySelectorAll(`div.${commentsClass}`)
+            const seeNext = Array.prototype.slice.call(commentNodes)
+              .find(c => c.id.includes('see_next'))
+            if (seeNext) {
+              const link = seeNext.querySelector('a')
+              return link && link.href
+            }
+            return null
+          }, commentsClass)
+          if (seeNextLink) {
+            // console.log('found see next link.', seeNextLink)
+            await Promise.all([
+              detailPage.waitForNavigation(),
+              detailPage.goto(seeNextLink)
+            ])
+          } else {
+            // we counted the number of pages wrong. :/
+            // console.log('expected to find see next link but there is none')
+            break
+          }
+        }
+        // console.log('number of comments: ', allComments.length)
+
+        const data = {
+          title: postTitle,
+          text: postText,
+          comments: allComments
         }
 
         // see if exists already.
         const searchParams = {
-          title: post.title,
-          text: post.text
+          title: postTitle,
+          text: postText
         }
 
         const res = await dbPosts.updateOne(
           searchParams,
-          { $set: post },
+          { $set: data },
           { upsert: true }
         )
         const { modifiedCount, upsertedCount, matchedCount } = res
@@ -168,14 +271,21 @@ const tally = {
       }
 
       // click the see more button to load a new page of posts
-      await page.evaluate(() => {
+      const seeMoreLink = await page.evaluate(() => {
         const seeMore = Array.prototype.slice.call(document.querySelectorAll('a'))
           .find(a => a.querySelector('span') && a.querySelector('span').textContent === 'See more posts')
-        seeMore.click()
+        return seeMore.href
       })
+
+      await Promise.all([
+        detailPage.waitForNavigation(),
+        detailPage.goto(seeMoreLink)
+      ])
+
       // end of loop through number of pages
       console.log(`page number ${ii + 1} completed.`)
       console.log(`${tally.total} in total, ${tally.added} added, ${tally.updated} updated, ${tally.matched} matched.`)
+      console.log('\n ******************* \n')
      }
   } catch (err) {
     console.log(err)
